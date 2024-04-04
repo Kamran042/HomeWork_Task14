351fa5a44454bfff00e1192e35dfd653ebde4aa4
Kamran042 <tu6gsu4nn@code.edu.az>
2024-04-04 23:17:47 +0400

    finish code comit

--- main.js
+++ main.js
@@ -77,21 +77,21 @@
 //? arrayin elementləri arasında minimum və maksimum elementi nəzərə almadan
 //? yerdə qalan elementlərin cəmini tapan function
 
-// let arr = [123,7,53,99];
-// let max = arr[0];
-// let min = arr[0];
-// let sum = 0;
-// for (let i = 1; i < arr.length; i++) {
-//     if (arr[i] > max) {
-//         max = arr[i];
-//     }
-//     if (arr[i] < min) {
-//         min = arr[i];
-//     }
-// }
-// for (let i = 0; i < arr.length; i++) {
-//     if (arr[i] !== min && arr[i] !== max) {
-//         sum += arr[i];
-//     }
-// }
-// console.log(sum);
+let arr = [123,7,53,99];
+let max = arr[0];
+let min = arr[0];
+let sum = 0;
+for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+        max = arr[i];
+    }
+    if (arr[i] < min) {
+        min = arr[i];
+    }
+}
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    let total = sum - max - min
+    
+}
+console.log(total);
